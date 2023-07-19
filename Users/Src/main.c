8975b068baec5ec306f411d03e6b0fc4bb9f1418
ncy <n15978654429@gmail.com>
2023-07-19 21:08:50 +0800

    删除部分注释

--- Users/Src/main.c
+++ Users/Src/main.c
@@ -6,14 +6,14 @@ uint8_t RoadLine;
 
 int main(void)
 {   OLED_Init();
-	SysTick_Init();//�δ�ʱ����ʼ��	
-	NVIC_Config();//�ж����ȼ�����2
-	Usart1_Init(115200);//115200-8-N-1
-	Tim3_Init();//��ʱ��3-10ms��ʱ
-	TIM1_PWM_Init(0,7199);//10khz�������
-	TIM2_Encoder_Init();//��ʱ��2����������
-	TIM4_Encoder_Init();//��ʱ��4����������
-	Motor_GPIO_Init();//�����Ӧ���ų�ʼ��
+	SysTick_Init();
+	NVIC_Config();
+	Usart1_Init(115200);
+	Tim3_Init();
+	TIM1_PWM_Init(0,7199);
+	TIM2_Encoder_Init();
+	TIM4_Encoder_Init();
+	Motor_GPIO_Init();
 OLED_ShowString(1, 2, "sipeed");
 while(1)
 {
