48952950174ba532a8ec9ffa46bb4da1968ceacc
legacyyl <1525700283@qq.com>
2023-07-21 21:04:51 +0800

    移植不小心增加了一个文件夹（不知道为什么在另一移植的时候文件是正常颜色）

---
+++ cmake-build-debug/CMakeFiles/MYCAR-CLION.elf.dir/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c.obj.d
@@ -0,0 +1,19 @@
+CMakeFiles/MYCAR-CLION.elf.dir/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c.obj: \
+ D:\Workspace\my-cars\FreeRTOS\Source\portable\GCC\ARM_CM3\port.c \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/FreeRTOS.h \
+ d:\gcc-arm-none-eabi\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ d:\gcc-arm-none-eabi\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\stdint.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\machine\_default_types.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\sys\features.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\_newlib_version.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\sys\_intsup.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\sys\_stdint.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/FreeRTOSConfig.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/projdefs.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/portable.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/deprecated_definitions.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\portable\GCC\ARM_CM3/portmacro.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/mpu_wrappers.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/task.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/list.h
---
+++ cmake-build-debug/CMakeFiles/MYCAR-CLION.elf.dir/FreeRTOS/Source/timers.c.obj.d
@@ -0,0 +1,35 @@
+CMakeFiles/MYCAR-CLION.elf.dir/FreeRTOS/Source/timers.c.obj: \
+ D:\Workspace\my-cars\FreeRTOS\Source\timers.c \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\stdlib.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\machine\ieeefp.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\_ansi.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\newlib.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\_newlib_version.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\sys\config.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\sys\features.h \
+ d:\gcc-arm-none-eabi\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\sys\reent.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\_ansi.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\sys\_types.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\machine\_types.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\machine\_default_types.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\sys\lock.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\sys\cdefs.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\machine\stdlib.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\alloca.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/FreeRTOS.h \
+ d:\gcc-arm-none-eabi\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\stdint.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\sys\_intsup.h \
+ d:\gcc-arm-none-eabi\arm-none-eabi\include\sys\_stdint.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/FreeRTOSConfig.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/projdefs.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/portable.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/deprecated_definitions.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\portable\GCC\ARM_CM3/portmacro.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/mpu_wrappers.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/task.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/list.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/queue.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/timers.h \
+ D:\Workspace\my-cars\FreeRTOS\Source\include/task.h
